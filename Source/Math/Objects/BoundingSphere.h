80cc00f075c8594ca6f6d9628215649e5da0880a
Sezz <sezzary@outlook.com>
2025-10-31 23:07:54 +1100

    Add `BoundingSphere` constructor

--- Source/Math/Objects/BoundingSphere.h
+++ Source/Math/Objects/BoundingSphere.h
@@ -16,17 +16,45 @@ namespace Silent::Math
         Vector3 Center = Vector3::Zero;
         float   Radius = 1.0f;
 
+        // =============
         // Constructors
+        // =============
 
+        /** @brief Constructs a default `BoundingSphere`. */
         constexpr BoundingSphere() = default;
+
+        /** @brief Constructs a `BoundingSphere` from a center and radius.
+         *
+         * @param center Sphere center.
+         * @param radius Sphere radius.
+         */
         constexpr BoundingSphere(const Vector3& center, float radius) : Center(center), Radius(radius) {}
 
-        // Getters
+        /** @brief Constructs a `BoundingSphere` from a collection of points.
+         *
+         * @param points Points to encompass with the sphere.
+         */
+        BoundingSphere(const std::span<const Vector3>& points);
 
+        // ========
+        // Getters
+        // ========
+
+        /** @brief Gets the surface area of the sphere.
+         *
+         * @return Surface area.
+         */
         float GetSurfaceArea() const;
+
+        /** @brief Gets the volume of the sphere.
+         *
+         * @return Volume.
+         */
         float GetVolume() const;
 
+        // ==========
         // Inquirers
+        // ==========
 
         bool Intersects(const Vector3& point) const;
         bool Intersects(const BoundingSphere& sphere) const;
@@ -38,11 +66,19 @@ namespace Silent::Math
         ContainmentType Contains(const AxisAlignedBoundingBox& aabb) const;
         ContainmentType Contains(const OrientedBoundingBox& obb) const;
 
+        // ===========
         // Converters
+        // ===========
 
+        /** Converts the sphere to an AABB.
+         *
+         * @return AABB encompassing the sphere.
+         */
         AxisAlignedBoundingBox ToAabb() const;
 
+        // ==========
         // Operators
+        // ==========
 
         bool            operator==(const BoundingSphere& sphere) const;
         bool            operator!=(const BoundingSphere& sphere) const;

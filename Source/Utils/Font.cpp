dbeae6ad14e04afbad33eb2ff42938116fc0e1ad
Sezz <sezzary@outlook.com>
2025-11-02 02:21:08 +1100

    Font progress

--- Source/Utils/Font.cpp
+++ Source/Utils/Font.cpp
@@ -1,7 +1,96 @@
 #include "Framework.h"
 #include "Utils/Font.h"
 
+#include "Application.h"
+#include "Renderer/Renderer.h"
+
 namespace Silent::Utils
 {
-    
+    Font::Font(FT_Library& fontLib, const std::filesystem::path& fontPath, int pointSize)
+    {
+        if (FT_Init_FreeType(&fontLib))
+        {
+            Debug::Log("Failed to initialize font library.", Debug::LogLevel::Error);
+            return;
+        }
+
+        if (FT_New_Face(fontLib, fontPath.string().c_str(), 0, &_fontFace))
+        {
+            Debug::Log("Failed to initialize font face.", Debug::LogLevel::Error);
+            return;
+        }
+
+        if (FT_Set_Pixel_Sizes(_fontFace, 0, pointSize))
+        {
+            Debug::Log("Failed to set font point size.", Debug::LogLevel::Error);
+            return;
+        }
+
+        _isLoaded = true;
+    }
+
+    Font::~Font()
+    {
+        FT_Done_Face(_fontFace);
+    }
+
+    std::vector<Glyph*> Font::GetGlyphs(const std::string& msg)
+    {
+        auto glyphs = std::vector<Glyph*>{};
+
+        for (auto thing : msg)
+        {
+
+        }
+    }
+
+    bool Font::IsLoaded() const
+    {
+        return _isLoaded;
+    }
+
+    FontManager::FontManager()
+    {
+
+    }
+
+    FontManager::~FontManager()
+    {
+        _fonts.clear();
+        FT_Done_FreeType(_fontLibrary);
+    }
+
+    Font* FontManager::GetFont(const std::string& fontName)
+    {
+        // Check if font exists.
+        auto it = _fonts.find(fontName);
+        if (it == _fonts.end())
+        {
+            Debug::Log("Attempted to get missing font `"+ fontName + "`.", Debug::LogLevel::Warning);
+            return nullptr;
+        }
+
+        auto& [keyFontName, font] = *it;
+        return &font;
+    }
+
+    void FontManager::LoadFont(const std::filesystem::path& fontPath, int pointSize)
+    {
+        // Check if font is already loaded.
+        auto fontName = fontPath.filename().string();
+        auto it       = _fonts.find(fontName);
+        if (it == _fonts.end())
+        {
+            Debug::Log("Attempted to load loaded font `"+ fontName + "`.", Debug::LogLevel::Warning, Debug::LogMode::Debug);
+            return;
+        }
+
+        // Handle load.
+        _fonts[fontName] = Font(_fontLibrary, fontPath, pointSize);
+        if (!_fonts[fontName].IsLoaded())
+        {
+            Debug::Log("Failed to load font `" + fontName + "`.", Debug::LogLevel::Error);
+            _fonts.erase(fontName);
+        }
+    }
 }
--- Source/Utils/Font.h
+++ Source/Utils/Font.h
@@ -2,21 +2,94 @@
 
 namespace Silent::Utils
 {
+    struct Glyph
+    {
+        int      Id       = 0;
+        Vector2i Position = Vector2i::Zero;
+        Vector2i Size     = Vector2i::Zero;
+        Vector2i Bearing  = Vector2i::Zero;
+        int      Advance  = 0;
+    };
+
     class Font
     {
     private:
-        
+        // =======
+        // Fields
+        // =======
+
+        FT_Face                        _fontFace = {};
+        std::unordered_map<int, Glyph> _glyphs   = {}; /** Key = glyph ID, value = glyph info. */
+        bool                           _isLoaded = false;
+
     public:
         // =============
         // Constructors
         // =============
 
-        Font() = default;
+        Font(FT_Library& fontLib, const std::filesystem::path& fontPath, int pointSize);
+
+        ~Font();
+
+        // ========
+        // Getters
+        // ========
+
+        std::vector<Glyph*> GetGlyphs(const std::string& msg);
+
+        // ==========
+        // Inquirers
+        // ==========
+
+        bool IsLoaded() const;
 
         // ==========
         // Utilities
         // ==========
 
-        void Load(const std::filesystem::path& fontPath);
+        void CacheGlyphs(const std::string& glyphs);
+
+        void UncacheGlyphs();
+
+    private:
+        // ========
+        // Helpers
+        // ========
+
+        void LoadGlyph(int glyphId);
+
+        void BuildAtlas();
+    };
+
+    class FontManager
+    {
+    private:
+        // =======
+        // Fields
+        // =======
+
+        FT_Library                            _fontLibrary = {};
+        std::unordered_map<std::string, Font> _fonts       = {}; /** Key = font name, value = font. */
+
+    public:
+        // =============
+        // Constructors
+        // =============
+
+        FontManager();
+
+        ~FontManager();
+
+        // ========
+        // Getters
+        // ========
+
+        Font* GetFont(const std::string& fontName);
+
+        // ==========
+        // Utilities
+        // ==========
+
+        void LoadFont(const std::filesystem::path& fontPath, int pointSize);
     };
 }
